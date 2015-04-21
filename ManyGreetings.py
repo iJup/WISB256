0c958939aa1a845328e773f7e3766ffb8ab59747
Jurriaan Parie <Jurriaan@MacBook-Pro-van-Jurriaan.local>
2015-04-21 12:40:24 +0200

    Eindversie

--- ManyGreetings.py
+++ ManyGreetings.py
@@ -1,3 +1,3 @@
-naam = input('ame?')
-aantal_greetings = input('How many greetings?')
+naam = input('What is your name? ')
+aantal_greetings = input('How many greetings? ')
 print(aantal_greetings + 'x Hello,' + naam + '!')
