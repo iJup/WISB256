9f182d4c135fca46f7b706eea7bdd1abfb41129d
Jurriaan Parie <Jurriaan@MacBook-Pro-van-Jurriaan.local>
2015-04-30 14:03:30 +0200

    Eindversie Many Greetings

--- Opdracht1/ManyGreetings.py
+++ Opdracht1/ManyGreetings.py
@@ -1,3 +1,3 @@
 naam = input('What is your name? ')
 aantal_greetings = input('How many greetings? ')
-print(aantal_greetings + 'x Hello,' + naam + '!')
+print(aantal_greetings + ' x Hello, ' + naam + '!')
