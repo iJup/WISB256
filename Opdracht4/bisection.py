a51c4be54893eb20336b124620f3b4be51ce6217
Jurriaan Parie <Jurriaan@MacBook-Pro-van-Jurriaan.local>
2015-05-26 11:06:52 +0200

    findAllRoots

--- Opdracht4/bisection.py
+++ Opdracht4/bisection.py
@@ -1,3 +1,6 @@
+from math import floor
+import math
+
 def findRoot(f,a,b,epsilon):
     a, b = [min(a,b),max(a,b)]
     m = (a + b)/2
@@ -14,3 +17,14 @@ def findRoot(f,a,b,epsilon):
     elif f(m) == 0:
         return m
             
+
+def findAllRoots(f,a,b,epsilon):
+    ls = []
+    n = (b-a) / epsilon
+    for i in range(floor(n)):
+        x, y = [a+(i-1) * epsilon, a + (i+epsilon) *epsilon]
+        if (f(x)*f(y) < 0):
+            root = findRoot(f,x,y,epsilon)
+            if root != None:
+                ls.append(root)
+    return ls
